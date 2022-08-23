980aa401da45dda7f69c905b7cd3609cb74915ef
Victoria Probst <victoria.probst13@gmail.com>
2022-08-23 14:39:41 -0400

    Established Database Connection in MySQL plus created Model structure

--- socialpage_project/local_settings.py
+++ socialpage_project/local_settings.py
@@ -5,7 +5,8 @@ SECRET_KEY = 'django-insecure-7=2c9442a0m7$lz$eogq)to1r5_wly)@df%%ku0(r9ovhcy29n
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'socialfeed_database',
+        'NAME': 'socialpage_database',
+        'HOST': 'localhost',
         'USER': 'root', 
         'PASSWORD': 'PASSword2022'
     }
--- socialpage_project/settings.py
+++ socialpage_project/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'page',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from socialpage_project.local_settings import *
+except ImportError:
+    pass
