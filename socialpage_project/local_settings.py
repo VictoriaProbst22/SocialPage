a68e0eb1f287d4726ec2091e36dc0cff55cb1c78
Victoria Probst <victoria.probst13@gmail.com>
2022-08-23 14:18:07 -0400

    Created New Project plus installed MySQL and Django REST Framework

---
+++ socialpage_project/local_settings.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-7=2c9442a0m7$lz$eogq)to1r5_wly)@df%%ku0(r9ovhcy29n'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialfeed_database',
+        'USER': 'root', 
+        'PASSWORD': 'PASSword2022'
+    }
+}
